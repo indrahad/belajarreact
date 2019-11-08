c792ecc758aca3ea37a8d0ce8e5605047c398df3
indrahad <purnama.indra96@gmail.com>
2019-11-08 19:02:26 +0700

    useState untuk merubah data

--- src/App.js
+++ src/App.js
@@ -1,34 +1,50 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
- state = {
-   persons: [
-   { name: 'Maxx', age: 50},
-   { name: 'Manu', age: 70},
-   { name: 'stephani', age: 30}  
-   ]
+  state = {
+    persons: [
+      { name: 'Maxx', age: 50 },
+      { name: 'Manu', age: 70 },
+      { name: 'stephani', age: 30 }
+    ],
+    otherState: 'some other value'
   }
 
   //ini adalah function dari tombol switch name
-  switchNameHandeler = () => {
-    console.log('Was Clicked!');
+  switchNameHandeler = (newName) => {
+    console.log('Was Clicked!'); //cek di console untuk mengetahuinya
+    // salah #this.state.persons[0].name = 'Mahmudi'
+    this.setState({
+      persons: [
+        { name: newName, age: 50 },
+        { name: 'Manu', age: 70 },
+        { name: 'stephani', age: 40 }
+      ]
+    })
   }
 
- render() {
-  return (
-    <div className="App">
+  render() {
+    return (
+      <div className="App">
         <h1>Hello World</h1>
         <p>Ini bekerja dengan baik</p>
-        <button onClick={this.switchNameHandeler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies : Racing </Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-    </div>
-  );
-}
+        <button onClick={() => this.switchNameHandeler()}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandeler.bind(this, 'Maxxxx')} > My Hobbies : Racing </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
+      </div>
+    );
+  }
 }
 
 export default App;
